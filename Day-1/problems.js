ed378587a00aa4e09a17e70bd30b5446eba6efa8
mb-tousif <meta.block.tousif@gmail.com>
2023-03-16 10:35:24 +0600

    First problem

---
+++ Day-1/problems.js
@@ -0,0 +1,12 @@
+/**
+ * Write a function to determine the largest difference between any two numbers
+ * in an array of numbers
+ */
+
+let numbers = [12, 5, 6, 9, 10, 2, 12, 2, 6, 5, 9, 10, 33];
+
+let difference = (function (arr) {
+
+})(numbers);
+
+console.log(difference);
