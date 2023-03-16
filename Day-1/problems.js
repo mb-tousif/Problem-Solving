ef69c6c029323dd412db35b8b99b120911fb7560
mb-tousif <meta.block.tousif@gmail.com>
2023-03-16 11:00:51 +0600

    Ana-gram of string

--- Day-1/problems.js
+++ Day-1/problems.js
@@ -14,3 +14,21 @@ let difference = (function (arr) {
 })(numbers);
 
 console.log(difference);
+
+/**
+ * Write a function to determine if one String is a case-insensitive anagram 
+ * of another String.
+ */
+// ana-gram: a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman. 
+
+function isAnagram(str1, str2) {
+    // convert both strings to lowercase
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+    // sort both strings
+    str1 = str1.split('').sort().join('');
+    str2 = str2.split('').sort().join('');
+    // compare both strings
+    return str1 === str2;
+}
+
