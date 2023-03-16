3676fa2c7f3eb8c154f6edf91d1954f113046f54
mb-tousif <meta.block.tousif@gmail.com>
2023-03-16 10:54:20 +0600

    problem solved

--- Day-1/problems.js
+++ Day-1/problems.js
@@ -6,7 +6,11 @@
 let numbers = [12, 5, 6, 9, 10, 2, 12, 2, 6, 5, 9, 10, 33];
 
 let difference = (function (arr) {
-
+    // Remove duplicate elements from the array
+    let unique = [...new Set(arr)].sort((a, b) => a - b);
+    // console.log(unique);
+    // calculate the difference between the first and last element
+    return unique[unique.length - 1] - unique[0];
 })(numbers);
 
 console.log(difference);
