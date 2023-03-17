f9b4e88ca01d19db14ed5bd435dbc2a7731aacd9
mb-tousif <meta.block.tousif@gmail.com>
2023-03-17 19:35:52 +0600

    Theory Test in code

---
+++ JS_Theory_Test/TrickyQ.js
@@ -0,0 +1,14 @@
+// What will be the output of the following console.log?
+
+let obj = {
+    'a': 'three',
+    b: 4,
+    'c': "five",
+    a: 3,
+    'b': "four",
+    "c": 'five',
+    "a": "tre",
+    "b": 'fyra',
+    c: 5
+};
+console.log(obj);
