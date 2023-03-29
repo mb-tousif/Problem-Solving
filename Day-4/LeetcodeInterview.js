1b228d9ee244b2ebec95b942f5fd412c7debb45d
mb-tousif <meta.block.tousif@gmail.com>
2023-03-29 22:33:40 +0600

    google interview Q. from Leetcode

--- Day-4/LeetcodeInterview.js
+++ Day-4/LeetcodeInterview.js
@@ -62,3 +62,20 @@ const findMedianSortedArrays = (nums1, nums2) => {
     return nums.length % 2 !== 0 ? nums[mid] : (nums[mid] + nums[mid - 1]) / 2;
 }
 console.log(findMedianSortedArrays([1, 3], [2]));
+
+// Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string. If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original.
+// Answer: Method-1
+const reverseStr = (s, k) => {
+    let arr = s.split("");
+    for (let i = 0; i < arr.length; i += 2 * k) {
+        let start = i;
+        let end = Math.min(start + k - 1, arr.length - 1);
+        while (start < end) {
+            [arr[start], arr[end]] = [arr[end], arr[start]];
+            start++;
+            end--;
+        }
+    }
+    return arr.join("");
+}
+console.log(reverseStr("abcdefg", 2));
