63d1154af02977c6ea8bd0c2426ec455b624757a
mb-tousif <meta.block.tousif@gmail.com>
2023-03-30 11:08:22 +0600

    Detect Cycle

--- Day-5/LeetcodeProblem.js
+++ Day-5/LeetcodeProblem.js
@@ -1,7 +1,7 @@
 // Given the head of a singly linked list, return the middle node of the linked list. If there are two middle nodes, return the second middle node.
 // Answer: Method-1
 
-let head = [1, 2, 3, 4, 5];
+let head = [3,2,0,-4];
 const middleNode = (head) => {
     let slow = head;
     let fast = head;
@@ -20,3 +20,19 @@ const middleNodeV2 = (head) => {
     return head[mid];
 };
 console.log(middleNodeV2(head));
+
+// Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null. There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+// Answer: Method-1
+const detectCycle = (head) => {
+    let set = new Set();
+    while (head) {
+        if (set.has(head)) {
+            return head;
+        }
+        set.add(head);
+        head = head.next;
+    }
+    return null;
+};
+
+console.log(detectCycle(head));
