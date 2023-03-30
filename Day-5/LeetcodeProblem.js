b08a6ccb4c0b19d5dad266354d6d3e9bbc82c71d
mb-tousif <meta.block.tousif@gmail.com>
2023-03-30 10:43:04 +0600

    list middle node

---
+++ Day-5/LeetcodeProblem.js
@@ -0,0 +1,16 @@
+// Given the head of a singly linked list, return the middle node of the linked list. If there are two middle nodes, return the second middle node.
+// Answer: Method-1
+
+let head = [1, 2, 3, 4, 5];
+const middleNode = (head) => {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+};
+console.log(middleNode(head));
+
+
