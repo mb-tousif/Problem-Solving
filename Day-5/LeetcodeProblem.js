883f6cf1132ca7b7b9f86b9c3b3779e8ecea972a
mb-tousif <meta.block.tousif@gmail.com>
2023-03-31 21:45:43 +0600

    Reverse Int

--- Day-5/LeetcodeProblem.js
+++ Day-5/LeetcodeProblem.js
@@ -36,3 +36,15 @@ const detectCycle = (head) => {
 };
 
 console.log(detectCycle(head));
+
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0. Assume the environment does not allow you to store 64-bit integers (signed or unsigned). 
+// Answer: Method-1
+const reverse = (x) => {
+    let rev = 0;
+    while (x !== 0) {
+        rev = rev * 10 + x % 10;
+        x = parseInt(x / 10);
+    }
+    return (rev < Math.pow(-2, 31) || rev > Math.pow(2, 31) - 1) ? 0 : rev;
+};
+console.log(reverse(123));
