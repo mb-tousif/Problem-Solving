9e0b2fe7a5d858631c9a1eefb8476ba12972e78e
mb-tousif <meta.block.tousif@gmail.com>
2023-04-07 00:10:27 +0600

    random text color

--- Day-6/problems.js
+++ Day-6/problems.js
@@ -9,3 +9,15 @@ const randomColor = () => {
 };
 // document.body.style.backgroundColor = randomColor();
 console.log(randomColor());
+
+// Write a function that generate random color. Apply this function to text color.
+// Answer:
+const randomTextColor = () => {
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += Math.floor(Math.random() * 16).toString(16);
+    } 
+    return color;
+};
+// document.body.style.color = randomTextColor();
+console.log(randomTextColor());
